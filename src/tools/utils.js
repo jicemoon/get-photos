d8ec6d9a6142b66fd5e91162ec8ea3266ac39f32
reese <reese@ying31.com>
2020-04-03 14:19:21 +0400

    bug修复

--- src/tools/utils.js
+++ src/tools/utils.js
@@ -1,4 +1,6 @@
+const { resolve } = require('path');
 const fileRoot = require('../configs/const').photosRootFold;
+
 exports.getOutputFold = path => {
   const fold = path.replace(/^\//g, '').split(/\//)[0];
   return [resolve(fileRoot, fold), fold];
